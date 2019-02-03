b094a2ae302c7a3cc7612739f4d15998ec7053b0
Bruno Oliveira <brunohdbass@gmail.com>
2019-02-03 14:46:02 -0200

    add multicolor and quote refreshable

--- index.js
+++ index.js
@@ -1,13 +1,55 @@
-fetch('https://api.adviceslip.com/advice')
+const container = document.querySelector('.box__quote')
+const description = document.querySelector('.box__quote-description')
+
+const setQuote = () => fetch('https://api.adviceslip.com/advice')
   .then((response) => {
     document.body.classList.add('loaded')
     response.json()
       .then((object) => {
         const advice = object.slip.advice
-        const container = document.querySelector('.box__quote')
         console.log(advice)
         container.innerText = `"${advice}"`
       })
       .catch(e => console.error(e))
+
+    if (!response.ok) {
+      description.remove();
+    }
   })
   .catch(e => console.error(e))
+
+
+setQuote()
+
+container.addEventListener('click', () => {
+  document.body.classList.remove('loaded')
+  setQuote()
+})
+
+
+let current = 0
+const colors = [
+  '#65c6c4',
+  '#3c4f65',
+  '#834c69',
+  '#ffbe00',
+  '#f77754',
+  '#EEE',
+  '#40514e',
+  '#000',
+  '#c70039',
+  '#74b49b',
+  '#044343',
+  '#7d7d7d',
+  '#b55400',
+]
+
+document.body.addEventListener('click', (event) => {
+  if (!event.target.matches('a') && !event.target.matches('.box__quote')) {
+    document.body.style = `background-color: ${colors[current]}`
+    current++
+    if (current >= colors.length) {
+      current = 0
+    }
+  }
+}, true)
